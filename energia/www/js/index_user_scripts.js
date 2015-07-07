0a3a06e617790da6890391da054cbf9898dab914
fredy <fredycn7sa@gmail.com>
2015-07-07 18:52:25 -0500

    Restrengiendo las lecturas de datos

--- energia/www/js/index_user_scripts.js
+++ energia/www/js/index_user_scripts.js
@@ -16,8 +16,13 @@
     {
         var lecturaAn = document.getElementById('lec1');
         var lecturaAc = document.getElementById('lec2');
+        if(lecturaAc>lecturaAn){ 
         var resultado = (lecturaAn.value)*1 + (lecturaAc.value)*1;
-        document.getElementById('resulataEntra').innerHTML='El resultado es:'+'<div style="color:red">'+resultado+'</div>';
+        document.getElementById('resulataEntra').innerHTML='El resultado es:'+'<div style="color:yellow">'+resultado+'</div>';
+        }
+        else
+             document.getElementById('resulataEntra').innerHTML='Ingrese nuevamente los datos'+'<div style="color:red">'+resultado+'</div>';
+            
          activate_page("#mainpage"); 
     });
     
