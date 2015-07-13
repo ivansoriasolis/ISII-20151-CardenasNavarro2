7f745ce880f923e8a5e87daa7acdd8d4c6b7b5c1
fredy <fredycn7sa@gmail.com>
2015-07-13 10:46:53 -0500

    Boton cancelar

--- energia/www/js/index_user_scripts.js
+++ energia/www/js/index_user_scripts.js
@@ -20,19 +20,12 @@
         if((lecturaAc.value)*1 >= (lecturaAn.value)*1){ 
             var resultado = (lecturaAc.value)*1 - (lecturaAn.value)*1;
             var costo = resultado*0.56;
-           // var igv =costo*0.18;
+         
             document.getElementById('resulataEntra').innerHTML='El KHW consumido es:'+'<div style="color:red">'+resultado+' KW </div><br>'+'El pago a realizar es:'+'<div style="color:blue"> S/. '+costo+' </div> ;
             
                     
         }
-        
-                if((lecturaAc.value)*1 == (lecturaAn.value)*1){ 
-            var resultado = (lecturaAc.value)*1 - (lecturaAn.value)*1;
-            var costo = resultado+5;
-            document.getElementById('resulataEntra').innerHTML='El KHW consumido es:'+'<div style="color:red">'+resultado+' KW </div><br>'+'El pago minimo es:'+'<div style="color:blue"> S/. '+costo+' </div>';
-            
-                    
-        }
+                       
         else
         {
             document.getElementById('resulataEntra').innerHTML='Ingrese nuevamente los datos';
