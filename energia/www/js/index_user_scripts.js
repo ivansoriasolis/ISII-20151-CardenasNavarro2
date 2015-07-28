030419bb6e0d515cf5888359ca021e7e56a3975f
fredy <fredycn7sa@gmail.com>
2015-07-28 12:40:01 -0500

    mejoras en la pantalla principal

--- energia/www/js/index_user_scripts.js
+++ energia/www/js/index_user_scripts.js
@@ -4,22 +4,27 @@
  /*
    hook up event handlers 
  */
- function register_event_handlers()
- {
+function register_event_handlers()
+{
+    
     var nombreEquipo = null;
     var montoTotal = 0;
     var equipos = [];
     
-  
-	/*function limpiarFomIN(){
-			
+    /*
+        c -> cantidad de equipo
+        p -> watts que consume el equipo
+        t -> tiempo en horas
+        var montoTotalArtefactos = ((equipo.c*(equipo.p * equipo.t))/1000) * costoKWh;
+    var equipo = {
+        c: null,
+        p: null, 
+        t: null,
+        monto: null
     }
-	 document.getElementById('cancelar').addEventListener('click',limpiarFomIN,false);
     */
-		
 					
-					
-    $(document).on("click", "#butoncalcu", function(evt)
+    $(document).on("click", "#idbtnCalcular", function(evt)
     {
         var lecActual = document.getElementById('idLecturaActual');
         var lecAnterior = document.getElementById('idLecturaAnterior');
@@ -30,25 +35,26 @@
 		var primeros30kw = 8.82; 
 		var mantenimiento = 0.67;
 		var costoKWh = 0.6070;
-		
-        var cargofijo = 3.59;
-        var intereses = 0.08;
-        var energia = 0;  	
-		
 		var electrificacion = 0.42;
 		
-		*//Condiciones para restringir ingesos
-					var condicion1 = (lecAnterior.value)*1 < (lecActual.value)*1;
-        			var condicion2 = lecActual.value != '' && lecAnterior.value != '';
-        			var condicion = condicion1 && condicion2;
-		
-		
-		//la   condicion para determinar el consumo de menor de 30 kwh
-		 if(condicion)
+
+        var cargofijo = 3.59;
+        var intereses = 0.08;
+        var energia = 0;     
+        
+
+        //var condicion = ((lecAnterior.value)*1 < (lecActual.value)*1) && (lecActual.value != '' && lecAnterior.value != '');
+		var condicion1 = (lecAnterior.value)*1 < (lecActual.value)*1;
+        var condicion2 = lecActual.value != '' && lecAnterior.value != '';
+        var condicion = condicion1 && condicion2;
+        
+        if(condicion)
 		{     
             var diferenciaLectura = (lecActual.value)*1 - (lecAnterior.value)*1;
 
             if (diferenciaLectura<=30) {
+				
+				alert("USTED TIENE LA POSIBILIDAD DE AFILIARSE AL FISE");
 
                 energia = diferenciaLectura * costoKWh;
                 var costoAlumbrado= energia * alum_public;
@@ -56,28 +62,25 @@
                 var subtotal = energia + costoAlumbrado + cargofijo + mantenimiento;
                 var total = subtotal + subtotal * igv;
                 var TotalPago = total + electrificacion;
-                document.getElementById('idTextCosto').innerHTML='USTED CONSUMIO :'+'<div style="color:#8B4513">'+diferenciaLectura+' KW </div><br>'+'EL SUBTOTAL ES:'+'<div style="color:#B8860B"> S/. '+ subtotal + ' </div><br>' + 'EL PAGO A REALIZAR POR EL CONSUMO ES:'+'<div style="color:#DAA520">S/.'+TotalPago+'</div>';
+                document.getElementById('idTextCosto').innerHTML='USTED CONSUMIO :'+'<div style="color:#8B4513">'+diferenciaLectura+' KW </div><br>'+'EL SUBTOTAL ES:'+'<div style="color:#B8860B"> S/. '+ subtotal + ' </div><br>' + 'EL PAGO A REALIZAR POR EL CONSUMO ES:'+'<div style="color:#DAA520">S/.'+TotalPago+'</div><br>'+ 'NO SE OLVIDE QUE PAGE ANTES DE 28 DE CADA MES'+'<div style ="color:#FF4500"';
             }
-		
-			
-			else
+            else
             {
                 var nuevoresult = diferenciaLectura - 30;//calcula la diferencia
                 energia = nuevoresult * costoKWh;
                 var costoAlumbrado = energia * alum_public;
-                var subtotal = energia + costoAlumbrado + cargofijo + mantenimiento + primeros30kw;
+                var subtotal = (energia + costoAlumbrado + cargofijo + mantenimiento + primeros30kw)*1;
                 var Total = subtotal * igv;
                 var TotalPago = Total + electrificacion;
                 document.getElementById('idTextCosto').innerHTML='USTED HA CONSUMISO:'+'<div style="color:#32CD32">'+diferenciaLectura+' KW </div><br>'+'EL SUBTOTAL ES:'+'<div style="color:#00FF00 style=" font-size:15px"> S/. '+subtotal+' </div><br>'+'EL PAGO A REALIZAR POR EL CONSUMO ES:'+'<div style="color:#7CFC00">S/.'+TotalPago+'</div><br>'+'NO SE OLVIDE QUE PAGE ANTES DE 28 DE CADA MES'+'<div style ="color:#FF4500"' ;  
-            }     
-			
-			
-			        else
+            }            
+        }               
+        else
         {
             if (condicion2 == false) {
-                alert("Debe ingresar los campos de lectura");
+                alert("DEBE INGRESAR LOS CAMPOS");
             }else{
-                alert("Error de lectura: Lectura Actual es menor");
+                alert(" ERROR DE LECTURA: LA LECTURA ACTUAL ES MENOR");
             }
             //document.getElementById('idTextCosto').innerHTML='Ingrese nuevamente los datos';
             lecActual.value='';
@@ -88,91 +91,139 @@
     
     $(document).on("click", "#cancelar", function(evt)
     {
-			alert("Datos cancelados");
+			alert("DATOS CANCELADOS");
 			var lecActual = document.getElementById('idLecturaActual');
         	var lecAnterior = document.getElementById('idLecturaAnterior');		
 					
-		    document.getElementById('idTextCosto').innerHTML='Se canceló la operación';
+		    document.getElementById('idTextCosto').innerHTML='SE CANCELO LA OPERACION ';
             lecActual.value='';
             lecAnterior.value='';	
     });
 
-		
-		
-        if((lecturaAc.value)*1 > (lecturaAn.value)*1)
-		
-		
-		{ 
-            var resultado = (lecturaAc.value)*1 - (lecturaAn.value)*1;
-			
-		if(resultado*1 < 30)
-			{
-				 console.log('Uds tiene  la posibilidad de afiliarse al FISE');
-			
-			     var energia = resultado*0.6070;
-				var alumbradoP= resultado*0.0945;
-				var subtotal = energia+alumbradoP+cargofijo+mantenimiento;
-				var total=subtotal*0.18;
-				var TotalPago = total+electrificacion;
-				
-            document.getElementById('resulataEntra').innerHTML='USTED CONSUMIO :'+'<div 		   style="color:#8B4513">'+resultado+' KW </div><br>'+'EL SUBTOTAL ES:'+'<div style="color:#B8860B"> S/. '+subtotal+' </div><br>'+'EL PAGO A REALIZAR POR EL CONSUMO ES:'+'<div style="color:#DAA520">S/.'+TotalPago+'</div>' ;
-				
-			}
-			var nuevoresult = lecturaAc-30;//calcula la diferencia
-			var energia = nuevoresult*0.6070;
-			var alumbradoP = energia*0.0945;
-			var subtotal = energia+alumbradoP+cargofijo+mantenimiento+primeros30kw;
-			var Total =subtotal*0.18;
-			var TotalPago = Total+electrificacion;
-			
-			
-         
-            document.getElementById('resulataEntra').innerHTML='USTED HA CONSUMISO:'+'<div 		   style="color:#32CD32">'+resultado+' KW </div><br>'+'EL SUBTOTAL ES:'+'<div style="color:#00FF00 style=" font-size:15px"> S/. '+subtotal+' </div><br>'+'EL PAGO A REALIZAR POR EL CONSUMO ES:'+'<div style="color:#7CFC00">S/.'+TotalPago+'</div><br>'+'NO SE OLVIDE QUE PAGE ANTES DE 28 DE CADA MES'+'<div style ="color:#FF4500"' ;
-            
-                    
-        }
-		  else if((lecturaAc.value)*1==(lecturaAn.value)*1)
-		
-		
-		{ 
-			
-			
-			alert("USTED NO TUVO CONSUMO EN ESTA VEZ");
-            var resultado = lecturaAc.value*1;
+    $(document).on("click", "#idEquipoSonido", function(evt)
+    {   
+        var div = document.getElementById("idtxtnombre");
+        nombreEquipo = "Equipo de Sonido"
+        div.textContent = nombreEquipo;
 
-		    var subtotal = cargofijo+intereses+mantenimiento;
-			var Total =subtotal*0.18;
-			var TotalPago = Total+electrificacion;
-		 
-			//var pago1 = "hola";
-			
-         
-            document.getElementById('resulataEntra').innerHTML='SU CONSUMO ES:'+'<div 		   style="color:#000000">'+resultado+' KW </div><br>'+'El subtotal es:'+'<div style="color:blue"> S/. '+subtotal+' </div><br>'+'Pago a realizar por el consumo:'+'<div style="color:yellow">S/.'+ TotalPago+'</div>' ;
-            
-                    
-        }
-                       
-        else
-        {
-            document.getElementById('resulataEntra').innerHTML='Ingrese nuevamente los datos';
-            lecturaAc.value='';
-            lecturaAn.value='';
-        }
-         activate_page("#mainpage"); 
     });
-    
-        /* button  #cancelar */
-    $(document).on("click", "#cancelar", function(evt)
-    {
-			alert("Se  cancelara los dato");
-			var lecturaAn = document.getElementById('lec1');
-        	var lecturaAc = document.getElementById('lec2');		
-					
-		    document.getElementById('resulataEntra').innerHTML='Se cancelo la operacion';
-            lecturaAc.value='';
-            lecturaAn.value='';	
+
+    $(document).on("click", "#idLavadora", function(evt)
+    {   
+        var div = document.getElementById("idtxtnombre");
+        nombreEquipo = "Lavadora"
+        div.textContent = nombreEquipo;
+
     });
+
+    $(document).on("click", "#idbtnlaptop", function(evt)
+    {   
+        var div = document.getElementById("idtxtnombre");
+        nombreEquipo = "Laptop"
+        div.textContent = nombreEquipo;
+
+    });
+
+    $(document).on("click", "#idbtnrefri", function(evt)
+    {   
+        var div = document.getElementById("idtxtnombre");
+        nombreEquipo = "Refrigeradora"
+        div.textContent = nombreEquipo;
+
+    });
+    $(document).on("click", "#idbtndvd", function(evt)
+    {   
+        var div = document.getElementById("idtxtnombre");
+        nombreEquipo = "Equipo DVD"
+        div.textContent = nombreEquipo;
+
+    });
+
+    $(document).on("click", "#idbtntv", function(evt)
+    {   
+        var div = document.getElementById("idtxtnombre");
+        nombreEquipo = "Televisor"
+        div.textContent = nombreEquipo;
+
+    });
+
+    $(document).on("click", "#idbtnplancha", function(evt)
+    {   
+        var div = document.getElementById("idtxtnombre");
+        nombreEquipo = "Plancha"
+        div.textContent = nombreEquipo;
+
+    });
+
+    $(document).on("click", "#idbtnducha", function(evt)
+    {   
+        var div = document.getElementById("idtxtnombre");
+        nombreEquipo = "Ducha Eléctrica"
+        div.textContent = nombreEquipo;
+
+    });
+
+    $(document).on("click", "#idbtngame", function(evt)
+    {   
+        var div = document.getElementById("idtxtnombre");
+        nombreEquipo = "Video Juego"
+        div.textContent = nombreEquipo;
+
+    });
+
+    $(document).on("click", "#idbtnfoco", function(evt)
+    {   
+        var div = document.getElementById("idtxtnombre");
+        nombreEquipo = "LED"
+        div.textContent = nombreEquipo;
+
+    });
+
+
+    $(document).on("click", "#idbtnAgregar", function(evt)
+    {   
+        var cantidad = document.getElementById('idcantidad');
+        var potencia = document.getElementById('idwatts');
+        var tiempo = document.getElementById('idhrs');
+           
+        equiposGasto(cantidad.value*1,potencia.value*1,tiempo.value*1);
+        
+    });
+
+
+    $(document).on("click", "#idbtnConsultar", function(evt)
+    {   
+        
+        for (var i = 0; i < equipos.length; i++) {
+            //document.getElementById('idtxtresultado').innerHTML='Cantidad de equipo:'+'<div style="color:red">'+equipos[i].c+'</div><br>'+'Potencia(watts):'+'<div style="color:blue">'+equipos[i].p+' </div><br>'+'Tiempo de uso:'+'<div style="color:yellow">'+equipos[i].t+' Hrs</div>'+'Costo por mes:'+'<div style="color:yellow">S/.'+equipos[i].monto+'</div>';
+            //$("#idtxtresultado").append('Cantidad de equipo:'+'<div style="color:red">'+equipos[i].c+'</div><br>'+'Potencia(watts):'+'<div style="color:blue">'+equipos[i].p+' </div><br>'+'Tiempo de uso:'+'<div style="color:yellow">'+equipos[i].t+' Hrs</div>'+'Costo por mes:'+'<div style="color:yellow">S/.'+equipos[i].monto+'</div>');
+            $("#idtxtresultado").append('<div style="color:blue">Nombre Equipo: '+equipos[i].nombre+'</div>'+'Cantidad de equipo:'+equipos[i].c+'<br>'+'Potencia(watts):'+equipos[i].p +'<br>'+'Tiempo de uso:'+ equipos[i].t+' Hrs<br>'+'<div style="color:blue">Costo por mes: S/.'+equipos[i].monto+'</div><br>');
+        };
+        $("#idtxtresultado").append('<div style="color:blue">Monto Total: S/.'+montoTotal+'</div>');
     
+    });
+
+
+    function equiposGasto(cantidad,potencia,tiempo){
+        
+        var costoKWh = 0.6070;
+        var montoTotalArtefacto = ((cantidad*(potencia * tiempo))/1000) * costoKWh;
+        
+        var equipo = {}
+        
+            equipo.nombre = nombreEquipo; 
+            equipo.c = cantidad;
+            equipo.p = potencia;
+            equipo.t = tiempo;
+            equipo.monto = montoTotalArtefacto;
+
+        equipos.push(equipo);
+
+        montoTotal += montoTotalArtefacto;
+        //alert(montoTotalArtefacto);
     }
- document.addEventListener("app.Ready", register_event_handlers, false);
+
+}
+document.addEventListener("app.Ready", register_event_handlers, false);
 })();
+
